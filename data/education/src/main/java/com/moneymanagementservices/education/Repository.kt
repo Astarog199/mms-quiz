52d22fcd90752d35f793503676fdf5f032a4ff1f
pkn <PKN199@yandex.ru>
2025-05-23 14:03:40 +0300

    v 0.5

--- data/education/src/main/java/com/moneymanagementservices/education/Repository.kt
+++ data/education/src/main/java/com/moneymanagementservices/education/Repository.kt
@@ -1,29 +1,13 @@
 package com.moneymanagementservices.education
 
 import com.moneymanagementservices.database.Education
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class Repository @Inject constructor(
-    private val coroutineDispatcher: CoroutineDispatcher,
     private val educationText: EducationText,
     private val localDataSource: LocalDataSource,
     private val mapper: Mapper
 ) {
-    private val scope = CoroutineScope(coroutineDispatcher)
-
-    private fun saveEducation(): Flow<List<Education>> {
-        scope.launch {
-            val entityList = educationText.toEntity().marginTrading.map(mapper::toEducation)
-            localDataSource.saveEducation(entityList)
-        }
-
-        return localDataSource.getEducations()
-    }
-
     fun showEducationTopics(): List<String> {
         return localDataSource.showEducationTopics()
     }
@@ -35,10 +19,17 @@ class Repository @Inject constructor(
             "Договора РЕПО" -> educationText.toEntity().REPO.map(mapper::toEducation)
             "Облигации" -> educationText.toEntity().bonds.map(mapper::toEducation)
             "Высокодоходные облигации с низким рейтингом (ВДО)" -> educationText.toEntity().bondsVDO.map(mapper::toEducation)
+            "Еврооблигации" -> educationText.toEntity().euroBonds.map(mapper::toEducation)
+            "Конвертируемые облигации" -> educationText.toEntity().convertibleBonds.map(mapper::toEducation)
             "Структурные облигации" -> educationText.toEntity().structuredBonds.map(mapper::toEducation)
             "Облигации со структурным доходом" -> educationText.toEntity().structuredIncomeBonds.map(mapper::toEducation)
             "Акции" -> educationText.toEntity().stock.map(mapper::toEducation)
             "Иностранные акции" -> educationText.toEntity().foreignStock.map(mapper::toEducation)
+            "Закрытые паевые инвестиционные фонды"  -> educationText.toEntity().zpif.map(mapper::toEducation)
+            "Акции не включённые в котировальные списки" -> mutableListOf()
+            "Паи и акции ETF, не включенные в котировальные списки, при наличии договора с биржей" -> mutableListOf()
+            "Облигации российских эмитентов выпущенные и регулируемые по праву иностранного государства" -> mutableListOf()
+            "Облигации иностранных эмитентов" -> mutableListOf()
             else -> mutableListOf()
         }
     }
--- data/education/src/main/java/com/moneymanagementservices/education/models/EntityList.kt
+++ data/education/src/main/java/com/moneymanagementservices/education/models/EntityList.kt
@@ -8,8 +8,15 @@ data class EntityList (
     @SerializedName("REPO_agreements") val REPO: List<Entity>,
     @SerializedName("bonds") val bonds: List<Entity>,
     @SerializedName("bonds_VDO") val bondsVDO: List<Entity>,
+    @SerializedName("euro_bonds") val euroBonds: List<Entity>,
+    @SerializedName("convertible_bonds") val convertibleBonds: List<Entity>,
     @SerializedName("structured_bonds") val structuredBonds: List<Entity>,
     @SerializedName("structured_income_bonds") val structuredIncomeBonds: List<Entity>,
     @SerializedName("stocks_market") val stock: List<Entity>,
-    @SerializedName("foreign_stock") val foreignStock: List<Entity>
+    @SerializedName("foreign_stock") val foreignStock: List<Entity>,
+    @SerializedName("closed_end_mutual_funds") val zpif: List<Entity>,
+    @SerializedName("shares_not_included_in_quotation_lists") val sharesNotIncludedQuotationLists: List<Entity>,
+    @SerializedName("ETF_theme_first") val ETFthemeFirst: List<Entity>,
+    @SerializedName("foreign_bonds") val foreignBonds: List<Entity>,
+    @SerializedName("bonds_governed_foreign_law") val bondsGovernedForeignLaw: List<Entity>
 )
